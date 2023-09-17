ec9b0babbadf07fd0d78e6582e6644d4e86f1e11
Marcos A. T <32946589+Tenebraw@users.noreply.github.com>
2023-09-17 06:59:11 -0300

    Recomposicion tarea 8.
    Se refactorizo la tarea de la clase 8, utilizando node.js y bootstrap. Utilizacion Tablas, layouts, grid, etc.

--- js/tarea-clase8.js
+++ js/tarea-clase8.js
@@ -1,10 +1,3 @@
-/*# Tarea clase 8
-
-A las 2 tareas de la clase 6, ponerles las validaciones que consideren
-necesarias.(usando RegExp, objetos, foreach, poner estilos, mostrar los erroes en la interfaz y escribir pruebas)
-
-TIP: Las edades no pueden tener decimales.
-*/
 
 function inicioProceso(){
     const $botonProceder = document.querySelector('#proceder');
@@ -14,14 +7,16 @@ function inicioProceso(){
     
             if(validarCantIntegrantes(cantidadDeIntegrantes,$inputCantidadDeIntegrantes)!=='')
             {
-                $inputCantidadDeIntegrantes.className='error';
-                document.querySelector('#errorProceder').innerHTML='* El campo no puede estar vacio y solo se aceptan numeros.';         
+                $inputCantidadDeIntegrantes.classList.add('error');
+                document.querySelector('#errorProceder').className='alert alert-danger';
+                document.querySelector('#errorProceder').innerHTML='El campo no puede estar vacio y solo se aceptan numeros.';         
             }
             else{
                 $botonProceder.setAttribute('disabled','');
                 $inputCantidadDeIntegrantes.setAttribute('disabled','');
-                $inputCantidadDeIntegrantes.className='';
+               $inputCantidadDeIntegrantes.classList.remove('error');
                 document.querySelector('#errorProceder').innerHTML='';
+                document.querySelector('#errorProceder').className='';
                 crearElementosInputs($botonProceder,cantidadDeIntegrantes,$inputCantidadDeIntegrantes);  
             }
             evento.preventDefault();
@@ -30,27 +25,66 @@ function inicioProceso(){
 
 function crearElementosInputs(boton,cantidad,inputs){
     
-    let $nuevosIntegrantes = document.querySelector('#nuevosintegrantes');
+    const tabla = document.querySelector('#tabla');
+    tabla.removeAttribute('hidden');
+    let $nuevosIntegrantes = document.querySelector('tbody');
     for(let i=0;i<cantidad;i++){
-        const newUsers = document.createElement('input');
-       const newbR = document.createElement('br');
-        const newLabel = document.createElement('label');
-        newUsers.name='edad';  
-        newLabel.textContent = ` Integrante nº ${i+1} `;
-        newUsers.type='number';
-        newUsers.min='01';
-        newUsers.max='100';
-        newUsers.placeholder='edad';
-        newUsers.className ='edadinput';
-        $nuevosIntegrantes.appendChild(newLabel);
-        $nuevosIntegrantes.appendChild(newUsers);
+        const newTr= document.createElement('tr');
+        const newTh= document.createElement('th');
+        newTh.scope='row';
+        newTh.textContent=`${i+1}`;
+        newTr.appendChild(newTh);
+
+        const newTd= document.createElement('td');
+        const newInputEdad = document.createElement('input');
+        newInputEdad.type='number';
+        newInputEdad.name='edad';
+        newInputEdad.min='01';
+        newInputEdad.max='100';
+        newInputEdad.className='form-control';
+        newTd.appendChild(newInputEdad);
+        newTr.appendChild(newTd);
+
+        const newTd2 = document.createElement('td');
+        const newButtonAgregar = document.createElement('button');
+        newButtonAgregar.type='button';
+        newButtonAgregar.className='btn btn-outline-secondary';
+        newButtonAgregar.innerHTML='Agregar';
+        newTd2.appendChild(newButtonAgregar);
+        newTr.appendChild(newTd2);
+
+        const newTd3 = document.createElement('td');
+        const newInputSalario = document.createElement('input');
+        newInputSalario.type='number';
+        newInputSalario.min='100';
+        newInputSalario.step='100';
+        newInputSalario.className='form-control salarioinput';
+        newInputSalario.disabled='true';
+        newTd3.appendChild(newInputSalario);
+        newTr.appendChild(newTd3);
+
+        const newTd4 = document.createElement('td');
+        const newButtonQuitar = document.createElement('button');
+        newButtonQuitar.type='button';
+        newButtonQuitar.className='btn btn-outline-secondary';
+        newButtonQuitar.id='quitar';
+        newButtonQuitar.innerHTML='Quitar';
+        newButtonQuitar.disabled='true';
+        newTd4.appendChild(newButtonQuitar);
+        newTr.appendChild(newTd4);
+
+        $nuevosIntegrantes.appendChild(newTr);   
+
+
 
  // Mostrando botones Calcular y Resetear. 
         const $botonCalcular = document.querySelector('#calcular');
         const $botonResetear = document.querySelector('#resetear');
+        const $botonSalario = document.querySelector('#calcularsalario');
+       
         $botonResetear.removeAttribute('hidden');
         $botonCalcular.removeAttribute('hidden');
-        
+        $botonSalario.removeAttribute('hidden');   
         $botonCalcular.onclick=function(evento){
             calculosEdad();
             evento.preventDefault();
@@ -59,70 +93,63 @@ function crearElementosInputs(boton,cantidad,inputs){
             BorrarCampos($nuevosIntegrantes,$botonCalcular,$botonResetear,boton,inputs,cantidad);
             evento.preventDefault();
         }
-
-        // Implementaciones tarea 2. Salarios. Creacion botones Agregar y Quitar.
-
-        const botonAgregarSalario = document.createElement('button');
-        botonAgregarSalario.textContent='Agregar Salario';
-        botonAgregarSalario.className='botonsalarioinput';
-        $nuevosIntegrantes.appendChild(botonAgregarSalario);
-        const inputsSalario = document.createElement('input');
-        inputsSalario.type='number';
-        inputsSalario.min='100';
-        inputsSalario.step='100';
-        inputsSalario.placeholder='salario';
-        inputsSalario.className ='salarioinput';
-        inputsSalario.setAttribute('hidden','');
-        $nuevosIntegrantes.appendChild(inputsSalario);
-
-        const botonQuitar = document.createElement('button');
-        botonQuitar.textContent='Quitar';
-        botonQuitar.className ='botonquitarsalario';
-        botonQuitar.setAttribute('hidden','');
-        $nuevosIntegrantes.appendChild(botonQuitar);
-
-        botonAgregarSalario.onclick=function(evento){
-            agregarSalario(inputsSalario);
-            agregarBotonQuitar(botonQuitar,inputsSalario);
+        $botonSalario.onclick=function(evento){
+            calculosSalarios();
             evento.preventDefault();
-        }           
-        $nuevosIntegrantes.appendChild(newbR);
+        }
+
+       newButtonAgregar.onclick=function(evento){
+        newInputSalario.removeAttribute('disabled');
+        newButtonQuitar.removeAttribute('disabled');
+        quitandoInputsSalario(newInputSalario,newButtonQuitar);
+        $botonSalario.className='btn btn-outline-primary';
+        evento.preventDefault();
+       }
     }
 }
 
 
-
 function mostrarResultadosEdad(x,y,z){
-    const $resultadosAmostrar = document.querySelector('#resultadosEdad');
-    $resultadosAmostrar.textContent =
-    `
-    La mayor edad es ${x}. 
-    La edad menor es ${y}. 
-    El promedio es de ${z}.
-    `;
-    return $resultadosAmostrar.removeAttribute('hidden');
+    const $resultadosEdad1 = document.querySelector('#mayoredad');
+    const $resultadosEdad2 = document.querySelector('#menoredad');
+    const $resultadosEdad3 = document.querySelector('#promedioedad');
+
+    $resultadosEdad1.textContent =` La mayor edad es ${x}.`;
+    $resultadosEdad2.textContent=` La edad menor es ${y}.`; 
+    $resultadosEdad3.textContent=`El promedio es de ${z}.`;
+
+    const cardEdad=document.querySelector('#resultadosedad');
+    cardEdad.removeAttribute('hidden');       
 }
 
-// Tarea 2
 function mostrarResultadosSalarios(x,y,z,v){
-    const $resultadosSalariosAmostrar = document.querySelector('#resultadosSalarios');
-    $resultadosSalariosAmostrar.textContent =
-    `
-    El mayor salario anual es ${x}. 
-    El menor salario anual es ${y}. 
-    Promedio salarios anuales es ${z}.
-    Promedio salario mensual es ${v};
-    `;
-    return $resultadosSalariosAmostrar.removeAttribute('hidden');
+    const $resultadosSalarios1 = document.querySelector('#mayorsalario');
+    const $resultadosSalarios2 = document.querySelector('#menorsalario');
+    const $resultadosSalarios3 = document.querySelector('#promedioanual');
+    const $resultadosSalarios4 = document.querySelector('#promediomensual');
+
+    $resultadosSalarios1.textContent =`El mayor salario anual es ${x}.`; 
+    $resultadosSalarios2.textContent=`El menor salario anual es ${y}.`;
+    $resultadosSalarios3.textContent=`Promedio salarios anuales es ${z}.`;
+    $resultadosSalarios4.textContent=`Promedio salario mensual es ${v}.`;
+
+    const cardSalario=document.querySelector('#resultadossalarios');
+    cardSalario.removeAttribute('hidden');   
 }
 
-// Al resetear borramos los campos correspondientes.
+function quitandoInputsSalario(newInputSalario,newButtonQuitar){
+      newButtonQuitar.onclick=function(evento){
+    newInputSalario.value='';
+    evento.preventDefault();
+     }
+}
+
+
 function BorrarCampos(nuevos,botonR,botonC,botonP,inputs){
+    document.querySelector('#resultadosedad').setAttribute('hidden','');
+    document.querySelector('#resultadossalarios').setAttribute('hidden','');
+    document.querySelector('#tabla').setAttribute('hidden','');  
     nuevos.innerHTML = "";
-    const resultadosAborrar = document.querySelector('#resultadosEdad');
-    const resultadosSalariosAborrar = document.querySelector('#resultadosSalarios');
-    resultadosAborrar.setAttribute('hidden','');
-    resultadosSalariosAborrar.setAttribute('hidden','');
     botonC.setAttribute('hidden','');
     botonR.setAttribute('hidden','');
     botonP.removeAttribute('disabled');
@@ -133,52 +160,22 @@ function BorrarCampos(nuevos,botonR,botonC,botonP,inputs){
     
 }
 
-// Tarea 2 Clase 6
-
-function agregarSalario(inputsS){
-    inputsS.removeAttribute('hidden');
-    const $botonCalcularSalario = document.querySelector('#calcularsalario');
-    $botonCalcularSalario.removeAttribute('hidden');
-    $botonCalcularSalario.onclick=function(evento){
-        calculosSalarios();
-        evento.preventDefault();
-    }
-}
-
-function agregarBotonQuitar(botonQ,inputsS){
-     botonQ.removeAttribute('hidden');
-     botonQ.onclick=function(evento){
-        inputsS.setAttribute('hidden','');
-        inputsS.value='';
-        botonQ.setAttribute('hidden','');
-        evento.preventDefault();
-     }    
-}
-
-/*# Tarea clase 8
-
-A las 2 tareas de la clase 6, ponerles las validaciones que consideren
-necesarias.(usando RegExp, objetos, foreach, poner estilos, mostrar los erroes en la interfaz y escribir pruebas)
-
-TIP: Las edades no pueden tener decimales.
-*/
-
 
 function validarCantIntegrantes(){
+    if (document.querySelector('#cantidad-integrantes').value.length === 0) {
+         return 'El campo integrantes no puede estar vacio';    
+     }
+     else {return '';}
+ }
 
-   if (document.querySelector('#cantidad-integrantes').value.length === 0) {
-        return 'El campo integrantes no puede estar vacio';    
-    }
-    else {return '';}
-}
-
-function validarEdades(nuevoArrayEdad){
+ function validarEdades(nuevoArrayEdad){
     let contador=0;
 
     nuevoArrayEdad.forEach(function(edad){
         if(edad.value.length==0){
-            edad.className='error';
-            document.querySelector('#errorEdad').innerHTML='* Ningun campo edad puede estar vacio.';
+            edad.classList.add('error');
+            document.querySelector('#errorEdad').innerHTML='Ningun campo edad puede estar vacio.';
+            document.querySelector('#errorEdad').className='alert alert-danger';
             contador++;
             return 'Ningun campo edad puede estar vacio';
         }
@@ -186,11 +183,13 @@ function validarEdades(nuevoArrayEdad){
     });
     if(contador===0){
         document.querySelector('#errorEdad').innerHTML='';
+        document.querySelector('#errorEdad').className='';
         nuevoArrayEdad.forEach(function(e){
-            e.className='edadinput';
+            e.className='form-control';
 
         });
         return '';
     }
 }
+
 inicioProceso();
